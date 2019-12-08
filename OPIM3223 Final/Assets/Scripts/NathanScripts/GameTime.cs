79375055d133522f9412c3764f41ba1458aff591
Nathan Silverman <nathan.silverman@uconn.edu>
2019-12-08 13:19:22 -0500

    Added Timer and grade prefabs

---
+++ OPIM3223 Final/Assets/Scripts/NathanScripts/GameTime.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameTime : MonoBehaviour
+{
+    public float TimeLeft = 10f;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        TimeLeft -= Time.deltaTime;
+        if (TimeLeft < 0)
+        {
+            SceneManager.LoadScene("MenuScene");
+
+        }
+    }
+}
