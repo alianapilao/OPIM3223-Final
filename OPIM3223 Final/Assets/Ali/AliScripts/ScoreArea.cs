530c051790a860c5781a8cd451a658145e6d2717
alianapilao <aliana.pilao@uconn.edu>
2019-12-08 13:35:33 -0500

    particle effect changes

---
+++ OPIM3223 Final/Assets/Ali/AliScripts/ScoreArea.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreArea : MonoBehaviour
+{
+    public GameObject effectObject;
+
+    void Start()
+    {
+        effectObject.SetActive(false);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.GetComponent<Ball>() != null)
+        {
+            effectObject.SetActive(true);
+        }
+    }
+}
